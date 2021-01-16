a2667ebd94e3baedbd103bb01571960eeec4d9cb
duwei <duweish@sinosoft.com.cn>
2021-01-16 17:40:00 +0800

    平安健康险系统初始化

--- ehis-hems-ui-admin/src/api/benefit/ruleconfig.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-// 查询分配产品字典
-export function getProductDict() {
-  return request({
-    url: '/product/definition/selectproductdict',
-    method: 'get',
-  })
-}

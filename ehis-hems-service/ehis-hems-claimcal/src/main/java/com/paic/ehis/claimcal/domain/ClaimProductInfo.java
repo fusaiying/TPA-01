a3080be05ef2595b91d5d268100c872c87cbe96a
傅赛赢 <1176040487@qq.com>
2021-02-05 20:10:28 +0800

    fsy

---
+++ ehis-hems-service/ehis-hems-claimcal/src/main/java/com/paic/ehis/claimcal/domain/ClaimProductInfo.java
@@ -0,0 +1,175 @@
+package com.paic.ehis.claimcal.domain;
+
+
+import com.paic.ehis.common.core.web.domain.BaseEntity;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.Date;
+
+/**
+ * 产品信息对象 claim_product
+ * 
+ * @author sino
+ * @date 2021-01-06
+ */
+public class ClaimProductInfo extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 产品编码 */
+    private String riskCode;
+
+    /** 产品名称 */
+    private String riskName;
+
+    /** 产品性质 */
+    private String riskNature;
+
+    /** 产品类别 */
+    private String riskType;
+
+    /** 产品分类 */
+    private String riskClass;
+
+    /** 产品状态 */
+    private String riskStatus;
+
+    /** 同步日期 */
+    private Date synchronizeTime;
+
+    /** 审核结论 */
+    private String examineConclusion;
+
+    /** 结论说明 */
+    private String conclusionExplanation;
+
+    /** 状态（Y-有效，N-无效） */
+    private String status;
+
+    /** 等待期 */
+    private String waitingPeriod;
+
+    /** 出单公司 */
+    private String company;
+
+    public String getRiskCode() {
+        return riskCode;
+    }
+
+    public void setRiskCode(String riskCode) {
+        this.riskCode = riskCode;
+    }
+
+    public String getRiskName() {
+        return riskName;
+    }
+
+    public void setRiskName(String riskName) {
+        this.riskName = riskName;
+    }
+
+    public String getRiskNature() {
+        return riskNature;
+    }
+
+    public void setRiskNature(String riskNature) {
+        this.riskNature = riskNature;
+    }
+
+
+    public String getRiskType() {
+        return riskType;
+    }
+
+    public void setRiskType(String riskType) {
+        this.riskType = riskType;
+    }
+
+    public String getRiskClass() {
+        return riskClass;
+    }
+
+    public void setRiskClass(String riskClass) {
+        this.riskClass = riskClass;
+    }
+
+    public String getRiskStatus() {
+        return riskStatus;
+    }
+
+    public void setRiskStatus(String riskStatus) {
+        this.riskStatus = riskStatus;
+    }
+
+
+    public Date getSynchronizeTime() {
+        return synchronizeTime;
+    }
+
+    public void setSynchronizeTime(Date synchronizeTime) {
+        this.synchronizeTime = synchronizeTime;
+    }
+
+    public String getExamineConclusion() {
+        return examineConclusion;
+    }
+
+    public void setExamineConclusion(String examineConclusion) {
+        this.examineConclusion = examineConclusion;
+    }
+
+    public String getConclusionExplanation() {
+        return conclusionExplanation;
+    }
+
+    public void setConclusionExplanation(String conclusionExplanation) {
+        this.conclusionExplanation = conclusionExplanation;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getWaitingPeriod() {
+        return waitingPeriod;
+    }
+
+    public void setWaitingPeriod(String waitingPeriod) {
+        this.waitingPeriod = waitingPeriod;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("riskCode", getRiskCode())
+            .append("riskName", getRiskName())
+            .append("riskNature", getRiskNature())
+            .append("riskType", getRiskType())
+            .append("riskClass", getRiskClass())
+            .append("riskStatus", getRiskStatus())
+            .append("synchronizeTime", getSynchronizeTime())
+            .append("examineConclusion", getExamineConclusion())
+            .append("conclusionExplanation", getConclusionExplanation())
+            .append("status", getStatus())
+            .append("waitingPeriod", getWaitingPeriod())
+            .append("company", getCompany())
+            .append("createBy", getCreateBy())
+            .append("createTime", getCreateTime())
+            .append("updateBy", getUpdateBy())
+            .append("updateTime", getUpdateTime())
+            .toString();
+    }
+}
---
+++ ehis-hems-service/ehis-hems-claimcal/src/main/java/com/paic/ehis/claimcal/service/IClaimProductPlanService.java
@@ -0,0 +1,64 @@
+package com.paic.ehis.claimcal.service;
+
+
+
+import com.paic.ehis.claimcal.domain.ClaimProductPlan;
+
+import java.util.List;
+
+/**
+ * 计划信息 Service接口
+ * 
+ * @author sino
+ * @date 2021-01-06
+ */
+public interface IClaimProductPlanService 
+{
+    /**
+     * 查询计划信息 
+     * 
+     * @param planCode 计划信息 ID
+     * @return 计划信息 
+     */
+    public ClaimProductPlan selectClaimProductPlanById(String planCode);
+
+    /**
+     * 查询计划信息 列表
+     * 
+     * @param claimProductPlan 计划信息 
+     * @return 计划信息 集合
+     */
+    public List<ClaimProductPlan> selectClaimProductPlanList(ClaimProductPlan claimProductPlan);
+
+    /**
+     * 新增计划信息 
+     * 
+     * @param claimProductPlan 计划信息 
+     * @return 结果
+     */
+    public int insertClaimProductPlan(ClaimProductPlan claimProductPlan);
+
+    /**
+     * 修改计划信息 
+     * 
+     * @param claimProductPlan 计划信息 
+     * @return 结果
+     */
+    public int updateClaimProductPlan(ClaimProductPlan claimProductPlan);
+
+    /**
+     * 批量删除计划信息 
+     * 
+     * @param planCodes 需要删除的计划信息 ID
+     * @return 结果
+     */
+    public int deleteClaimProductPlanByIds(String[] planCodes);
+
+    /**
+     * 删除计划信息 信息
+     * 
+     * @param planCode 计划信息 ID
+     * @return 结果
+     */
+    public int deleteClaimProductPlanById(String planCode);
+}

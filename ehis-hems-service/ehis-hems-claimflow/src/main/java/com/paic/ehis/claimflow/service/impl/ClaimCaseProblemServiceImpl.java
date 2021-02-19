4d027073eb6a079871d8e0396876bd0d07244782
zhuangch <zhuangcunhao@sinosoft.com.cn>
2021-02-19 09:53:50 +0800

    问题件bug修复

--- ehis-hems-service/ehis-hems-claimflow/src/main/java/com/paic/ehis/claimflow/service/impl/ClaimCaseProblemServiceImpl.java
+++ ehis-hems-service/ehis-hems-claimflow/src/main/java/com/paic/ehis/claimflow/service/impl/ClaimCaseProblemServiceImpl.java
@@ -1,8 +1,12 @@
 package com.paic.ehis.claimflow.service.impl;
 
 
+import com.paic.ehis.claimflow.domain.ClaimCase;
 import com.paic.ehis.claimflow.domain.ClaimCaseProblem;
+import com.paic.ehis.claimflow.domain.ClaimCaseRecord;
+import com.paic.ehis.claimflow.mapper.ClaimCaseMapper;
 import com.paic.ehis.claimflow.mapper.ClaimCaseProblemMapper;
+import com.paic.ehis.claimflow.mapper.ClaimCaseRecordMapper;
 import com.paic.ehis.claimflow.service.IClaimCaseProblemService;
 import com.paic.ehis.common.core.utils.DateUtils;
 import com.paic.ehis.common.core.utils.SecurityUtils;
@@ -23,6 +27,12 @@ public class ClaimCaseProblemServiceImpl implements IClaimCaseProblemService
 {
     @Autowired
     private ClaimCaseProblemMapper claimCaseProblemMapper;
+
+    @Autowired
+    private ClaimCaseRecordMapper claimCaseRecordMapper;
+
+    @Autowired
+    private ClaimCaseMapper claimCaseMapper;
 
     /**
      * 查询案件问题件
@@ -107,7 +117,35 @@ public class ClaimCaseProblemServiceImpl implements IClaimCaseProblemService
     @Override
     public int updateClaimCaseProblem(ClaimCaseProblem claimCaseProblem)
     {
-        claimCaseProblem.setUpdateTime(DateUtils.getNowDate());
+        ClaimCaseRecord claimCaseRecord = new ClaimCaseRecord();
+        claimCaseRecord.setRptNo(claimCaseProblem.getRptNo());
+        claimCaseRecord.setHistoryFlag("Y");
+        claimCaseRecord.setStatus("Y");
+        claimCaseRecord.setOperation("05");
+        ClaimCaseRecord caseRecord = claimCaseRecordMapper.selectRecentClaimCaseRecord(claimCaseRecord);
+        ClaimCase claimCase=claimCaseMapper.selectClaimCaseById(claimCaseProblem.getRptNo());
+        claimCase.setUpdateBy(caseRecord.getOperator());
+        claimCase.setUpdateTime(DateUtils.getNowDate());
+        claimCase.setCaseStatus("05");
+
+
+        ClaimCaseRecord record = claimCaseRecordMapper.selectClaimCaseRecordByrptNoFive(claimCaseProblem.getRptNo());
+        record.setOperator(SecurityUtils.getUsername());
+        record.setHistoryFlag("Y");
+        record.setUpdateBy(SecurityUtils.getUsername());
+        record.setUpdateTime(DateUtils.getNowDate());
+        claimCaseRecordMapper.updateClaimCaseRecord(record);
+
+        claimCaseRecord.setHistoryFlag("N");
+        claimCaseRecord.setOperation("05");
+        claimCaseRecord.setOrgRecordId(record.getRecordId());
+        claimCaseRecord.setCreateBy(SecurityUtils.getUsername());
+        claimCaseRecord.setCreateTime(DateUtils.getNowDate());
+        claimCaseRecordMapper.insertClaimCaseRecord(claimCaseRecord);
+
+        claimCaseMapper.updateClaimCase(claimCase);
+
+
         return claimCaseProblemMapper.updateClaimCaseProblem(claimCaseProblem);
     }
 

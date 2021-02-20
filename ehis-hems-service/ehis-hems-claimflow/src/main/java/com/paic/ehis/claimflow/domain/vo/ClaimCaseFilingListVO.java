010f23be52b121ccc53680341cdbcb35e60022f0
hjw <houjiawei@sinosoft.como.cn>
2021-02-20 15:26:28 +0800

    案件归档

--- ehis-hems-service/ehis-hems-claimflow/src/main/java/com/paic/ehis/claimflow/domain/vo/ClaimCaseFilingListVO.java
+++ ehis-hems-service/ehis-hems-claimflow/src/main/java/com/paic/ehis/claimflow/domain/vo/ClaimCaseFilingListVO.java
@@ -68,4 +68,115 @@ public class ClaimCaseFilingListVO extends BaseEntity {
     /** 报案号 */
     private String rptNo;
 
+    public String getCaseBoxNo() {
+        return caseBoxNo;
+    }
+
+    public void setCaseBoxNo(String caseBoxNo) {
+        this.caseBoxNo = caseBoxNo;
+    }
+
+    public String getBatchNo() {
+        return batchNo;
+    }
+
+    public void setBatchNo(String batchNo) {
+        this.batchNo = batchNo;
+    }
+
+    public String getRptStartNo() {
+        return rptStartNo;
+    }
+
+    public void setRptStartNo(String rptStartNo) {
+        this.rptStartNo = rptStartNo;
+    }
+
+    public String getRptEndNo() {
+        return rptEndNo;
+    }
+
+    public void setRptEndNo(String rptEndNo) {
+        this.rptEndNo = rptEndNo;
+    }
+
+    public String getClaimType() {
+        return claimType;
+    }
+
+    public void setClaimType(String claimType) {
+        this.claimType = claimType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDeptCode() {
+        return deptCode;
+    }
+
+    public void setDeptCode(String deptCode) {
+        this.deptCode = deptCode;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCasenum() {
+        return casenum;
+    }
+
+    public void setCasenum(String casenum) {
+        this.casenum = casenum;
+    }
+
+    public Date getUpdateStartTime() {
+        return updateStartTime;
+    }
+
+    public void setUpdateStartTime(Date updateStartTime) {
+        this.updateStartTime = updateStartTime;
+    }
+
+    public Date getUpdateEndTime() {
+        return updateEndTime;
+    }
+
+    public void setUpdateEndTime(Date updateEndTime) {
+        this.updateEndTime = updateEndTime;
+    }
+
+    public String getRptNo() {
+        return rptNo;
+    }
+
+    public void setRptNo(String rptNo) {
+        this.rptNo = rptNo;
+    }
 }

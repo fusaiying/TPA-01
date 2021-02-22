62e991bc381b5add32543df675e2211efa19e58c
李盈财 <1446639902@qq.com>
2021-02-22 11:52:54 +0800

    2021/02/22

--- ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/billingDetails.vue
+++ ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/billingDetails.vue
@@ -780,6 +780,18 @@
           callback(new Error("账单金额不能为空"));
         }
       }
+      const checkTreatmentType = (rule, value, callback) => {
+        if (value) {
+          if (this.baseForm.treatmentType === '1' && (this.baseForm.treatmentEndDate !== null || this.baseForm.treatmentEndDate !== '')) {
+            this.baseForm.treatmentDays = this.DateDiff(this.baseForm.treatmentEndDate, this.baseForm.treatmentStartDate) + 1
+          }
+          if (this.baseForm.treatmentType === '2' && (this.baseForm.treatmentEndDate !== null || this.baseForm.treatmentEndDate !== '')) {
+            this.baseForm.treatmentDays = this.DateDiff(this.baseForm.treatmentEndDate, this.baseForm.treatmentStartDate)
+          }
+        } else {
+          callback(new Error("治疗类型不能为空"));
+        }
+      }
       const checkTreatmentStartDate = (rule, value, callback) => {
         let date = new Date();
         let month = date.getMonth() + 1
@@ -944,7 +956,7 @@
           hospitalName: [{required: true, message: '就诊医院不能为空', trigger: ['blur', 'change']}],
           billCurrency: [{required: true, message: '账户币种不能为空', trigger: ['blur', 'change']}],
           billAmount: [{validator: checkBillAmount, required: true, trigger: ['blur', 'change']}],
-          treatmentType: [{required: true, message: '治疗类型不能为空', trigger: ['blur', 'change']}],
+          treatmentType: [{validator: checkTreatmentType,required: true, trigger: ['blur', 'change']}],
           treatmentStartDate: [{validator: checkTreatmentStartDate, required: true, trigger: ['blur', 'change']}],
           treatmentEndDate: [{validator: checkTreatmentEndDate, required: true, trigger: ['blur', 'change']}],
           treatmentDays: [{validator: checkTreatmentDays, required: true, trigger: ['blur', 'change']}],

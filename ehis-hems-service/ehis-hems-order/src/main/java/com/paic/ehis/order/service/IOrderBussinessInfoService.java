499f109cfacf5f1821cd77df595b6f2dc55d4e3b
汪洁 <wangjie007@sinosoft.com.cn>
2021-02-22 16:59:03 +0800

    B端工单接口新增

---
+++ ehis-hems-service/ehis-hems-order/src/main/java/com/paic/ehis/order/service/IOrderBussinessInfoService.java
@@ -0,0 +1,50 @@
+package com.paic.ehis.order.service;
+
+import com.paic.ehis.common.core.web.domain.AjaxResult;
+import com.paic.ehis.order.domain.*;
+
+import java.util.List;
+
+/**
+ * order_info(工单信息)Service接口
+ * 
+ * @author sino
+ * @date 2021-01-26
+ */
+public interface IOrderBussinessInfoService
+{
+    /**
+     * 验证登录账号和密码
+     * 
+     * @param roleLoginInfo
+     */
+    public AjaxResult checkpassWord(RoleLoginInfo roleLoginInfo);
+
+
+    /**
+     * 查询接单人拥有的接单数量
+     *
+     * @param role
+     */
+    public OrderNumInfo getOrderNum(String role);
+
+
+    /**
+     * 查询不同节点状态下的工单列表
+     */
+    public List<OrderInfo> getOrderList(RoleLoginInfo roleLoginInfo);
+
+
+    /**
+     * 接单操作
+     */
+    public int receivingOrder(RoleLoginInfo roleLoginInfo);
+
+
+    /**
+     * 实施操作
+     */
+    public int implementtOrder(OrderInfo orderInfo);
+
+
+}

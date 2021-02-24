98994ebb0d34de7dcb2b5b8b4ab783c6ada56216
李盈财 <1446639902@qq.com>
2021-02-24 11:13:15 +0800

    2021/02/24

--- ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/caseCalculate.vue
+++ ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/caseCalculate.vue
@@ -368,15 +368,13 @@
            let flag=true
            let flag2=true
             this.caseForm.caseData.forEach(item=>{
-               if (item.payAmount!=item.calAmount){
+               if (item.payAmount!=item.calAmount && (item.remark!=null && item.remark!='' && item.remark!=undefined )){
                  flag=false
               }
               if (item.payConclusion==null || item.payConclusion=='' || item.payConclusion==undefined ){
                 flag2=false
               }
             })
-            alert(flag)
-            alert(flag2)
             if (flag){
               if (flag2){
                 let item = {

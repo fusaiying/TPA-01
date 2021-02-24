e66db1e3dcb94243bfb42b6cd6a1aaf15b786adb
zhuangch <zhuangcunhao@sinosoft.com.cn>
2021-02-24 16:45:55 +0800

    TPA理赔受理信息接口

---
+++ ehis-hems-service/ehis-hems-claimflow/src/main/java/com/paic/ehis/claimflow/domain/dto/ClaimMoreSubDTO.java
@@ -0,0 +1,11 @@
+package com.paic.ehis.claimflow.domain.dto;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class ClaimMoreSubDTO {
+    private String customerNo;
+    private String docuno;
+}

4a04d887687972b9d325a0ea24f4b53be5f8d070
hjw <houjiawei@sinosoft.como.cn>
2021-02-25 17:11:02 +0800

    重置 清空

--- ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/discussion.vue
+++ ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/discussion.vue
@@ -208,7 +208,7 @@
             </el-col>
 
             <el-col :span="8">
-              <el-form-item label="提调机构：" prop="organCode" key="organCode1">
+              <el-form-item label="提调机构：" prop="organCode" key="organCode1da">
                 <el-select v-model="surveyForm.organCode" class="item-width" size="mini" placeholder="请选择">
                   <el-option v-for="item in initiateOrg" :key="item.dictValue"  :label="item.dictLabel + ' - ' +item.dictValue" :value="item.dictValue"/>
                 </el-select>
@@ -216,7 +216,7 @@
             </el-col>
 
             <el-col :span="8">
-              <el-form-item label="保单号：" prop="policyNo"  key="policyNo1">
+              <el-form-item label="保单号：" prop="policyNo"  key="policyNo1df">
                 <el-select v-model="surveyForm.policyNo" class="item-width" size="mini" placeholder="请选择">
                   <el-option v-for="dict in policyNos"
                              :key="dict.policyNo"
@@ -855,13 +855,14 @@
       },
       formReset(fromName){
      //   this.$refs[fromName].resetFields()
-        this.surveyForm.invType ='';
-        this.surveyForm.invReason ='';
-        this.surveyForm.invOrganCode ='';
+        this.surveyForm.invType = '';
+        this.surveyForm.invReason = '';
+        this.surveyForm.invOrganCode = '';
+        this.surveyForm.organCode = ' ';
+        this.surveyForm.policyNo = ' ';
+        this.surveyForm.invView = '';
         this.surveyForm.organCode = '';
-        this.surveyForm.policyNo ='';
-        this.surveyForm.invView ='';
-
+        this.surveyForm.policyNo = '';
         this.$refs['surveyForm'].clearValidate();
 
 

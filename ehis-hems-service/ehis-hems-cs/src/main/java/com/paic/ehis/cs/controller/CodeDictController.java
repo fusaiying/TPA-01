40d84161b9d5c1bde685f43731231dc400bb8613
zhuangch <zhuangcunhao@sinosoft.com.cn>
2021-02-27 17:52:14 +0800

    预约查询

--- ehis-hems-service/ehis-hems-cs/src/main/java/com/paic/ehis/cs/controller/CodeDictController.java
+++ ehis-hems-service/ehis-hems-cs/src/main/java/com/paic/ehis/cs/controller/CodeDictController.java
@@ -4,12 +4,10 @@ import java.util.List;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
-
-import com.paic.ehis.cs.domain.CodeEnum;
+import com.paic.ehis.cs.domain.CodeDict;
 import com.paic.ehis.cs.domain.dto.CodeEnumDTO;
 import com.paic.ehis.cs.domain.vo.CodeEnumVo;
-import com.paic.ehis.cs.service.IBasicServiceApplicationService;
-import com.paic.ehis.cs.service.ICodeEnumService;
+import com.paic.ehis.cs.service.ICodeDictService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,38 +30,38 @@ import com.paic.ehis.common.core.web.page.TableDataInfo;
  * 业务码 Controller
  * 
  * @author sino
- * @date 2021-02-23
+ * @date 2021-02-27
  */
 @RestController
-@RequestMapping("/enum")
-public class CodeEnumController extends BaseController
+@RequestMapping("/dict")
+public class CodeDictController extends BaseController
 {
     @Autowired
-    private ICodeEnumService codeEnumService;
+    private ICodeDictService codeDictService;
 
     /**
      * 查询业务码 列表
      */
-    @PreAuthorize("@ss.hasPermi('system:enum:list')")
+    @PreAuthorize("@ss.hasPermi('system:dict:list')")
     @GetMapping("/list")
-    public TableDataInfo list(CodeEnum codeEnum)
+    public TableDataInfo list(CodeDict codeDict)
     {
         startPage();
-        List<CodeEnum> list = codeEnumService.selectCodeEnumList(codeEnum);
+        List<CodeDict> list = codeDictService.selectCodeDictList(codeDict);
         return getDataTable(list);
     }
 
     /**
      * 导出业务码 列表
      */
-    @PreAuthorize("@ss.hasPermi('system:enum:export')")
+    @PreAuthorize("@ss.hasPermi('system:dict:export')")
     @Log(title = "业务码 ", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, CodeEnum codeEnum) throws IOException
+    public void export(HttpServletResponse response, CodeDict codeDict) throws IOException
     {
-        List<CodeEnum> list = codeEnumService.selectCodeEnumList(codeEnum);
-        ExcelUtil<CodeEnum> util = new ExcelUtil<CodeEnum>(CodeEnum.class);
-        util.exportExcel(response, list, "enum");
+        List<CodeDict> list = codeDictService.selectCodeDictList(codeDict);
+        ExcelUtil<CodeDict> util = new ExcelUtil<CodeDict>(CodeDict.class);
+        util.exportExcel(response, list, "dict");
     }
 
 
@@ -75,7 +73,7 @@ public class CodeEnumController extends BaseController
     @PostMapping("/exportNew")
     public void export(HttpServletResponse response, CodeEnumDTO codeEnumDTO) throws IOException
     {
-        List<CodeEnumVo> list = codeEnumService.selectCodeEnumVo(codeEnumDTO);
+        List<CodeEnumVo> list = codeDictService.selectCodeEnumVo(codeEnumDTO);
         ExcelUtil<CodeEnumVo> util = new ExcelUtil<CodeEnumVo>(CodeEnumVo.class);
         util.exportExcel(response, list, "enum");
     }
@@ -83,51 +81,50 @@ public class CodeEnumController extends BaseController
 
     @GetMapping("/updateCodeEnumVo")
     public AjaxResult updateCodeEnumVo(List<CodeEnumVo> list){
-        return AjaxResult.success(codeEnumService.updateCodeEnumVo(list));
+        return AjaxResult.success(codeDictService.updateCodeEnumVo(list));
     }
-
 
 
     /**
      * 获取业务码 详细信息
      */
-    @PreAuthorize("@ss.hasPermi('system:enum:query')")
-    @GetMapping(value = "/{codeId}")
-    public AjaxResult getInfo(@PathVariable("codeId") Long codeId)
+    @PreAuthorize("@ss.hasPermi('system:dict:query')")
+    @GetMapping(value = "/{codeType}")
+    public AjaxResult getInfo(@PathVariable("codeType") String codeType)
     {
-        return AjaxResult.success(codeEnumService.selectCodeEnumById(codeId));
+        return AjaxResult.success(codeDictService.selectCodeDictById(codeType));
     }
 
     /**
      * 新增业务码 
      */
-    @PreAuthorize("@ss.hasPermi('system:enum:add')")
+    @PreAuthorize("@ss.hasPermi('system:dict:add')")
     @Log(title = "业务码 ", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody CodeEnum codeEnum)
+    public AjaxResult add(@RequestBody CodeDict codeDict)
     {
-        return toAjax(codeEnumService.insertCodeEnum(codeEnum));
+        return toAjax(codeDictService.insertCodeDict(codeDict));
     }
 
     /**
      * 修改业务码 
      */
-    @PreAuthorize("@ss.hasPermi('system:enum:edit')")
+    @PreAuthorize("@ss.hasPermi('system:dict:edit')")
     @Log(title = "业务码 ", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody CodeEnum codeEnum)
+    public AjaxResult edit(@RequestBody CodeDict codeDict)
     {
-        return toAjax(codeEnumService.updateCodeEnum(codeEnum));
+        return toAjax(codeDictService.updateCodeDict(codeDict));
     }
 
     /**
      * 删除业务码 
      */
-    @PreAuthorize("@ss.hasPermi('system:enum:remove')")
+    @PreAuthorize("@ss.hasPermi('system:dict:remove')")
     @Log(title = "业务码 ", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{codeIds}")
-    public AjaxResult remove(@PathVariable Long[] codeIds)
+	@DeleteMapping("/{codeTypes}")
+    public AjaxResult remove(@PathVariable String[] codeTypes)
     {
-        return toAjax(codeEnumService.deleteCodeEnumByIds(codeIds));
+        return toAjax(codeDictService.deleteCodeDictByIds(codeTypes));
     }
 }
---
+++ ehis-hems-service/ehis-hems-cs/src/main/java/com/paic/ehis/cs/mapper/CodeDictMapper.java
@@ -0,0 +1,76 @@
+package com.paic.ehis.cs.mapper;
+
+import com.paic.ehis.cs.domain.CodeDict;
+import com.paic.ehis.cs.domain.dto.CodeEnumDTO;
+import com.paic.ehis.cs.domain.vo.CodeEnumVo;
+import com.paic.ehis.cs.domain.vo.CodeEnumVo1;
+
+import java.util.List;
+
+
+/**
+ * 业务码 Mapper接口
+ * 
+ * @author sino
+ * @date 2021-02-27
+ */
+public interface CodeDictMapper 
+{
+    /**
+     * 查询业务码 
+     * 
+     * @param codeType 业务码 ID
+     * @return 业务码 
+     */
+    public CodeDict selectCodeDictById(String codeType);
+
+    public List<CodeEnumVo> selectCodeEnumVoList(CodeEnumDTO codeEnumDTO);
+
+    /**
+     * 查询业务码 列表
+     * 
+     * @param codeDict 业务码 
+     * @return 业务码 集合
+     */
+    public List<CodeDict> selectCodeDictList(CodeDict codeDict);
+
+
+    public List<CodeDict> selectComplaintBusiness(CodeDict codeDict);
+
+    public List<CodeDict> selectInsuranceSource(CodeDict codeDict);
+
+    /**
+     * 新增业务码 
+     * 
+     * @param codeDict 业务码 
+     * @return 结果
+     */
+    public int insertCodeDict(CodeDict codeDict);
+
+    /**
+     * 修改业务码 
+     * 
+     * @param codeDict 业务码 
+     * @return 结果
+     */
+    public int updateCodeDict(CodeDict codeDict);
+
+    /**
+     * 删除业务码 
+     * 
+     * @param codeType 业务码 ID
+     * @return 结果
+     */
+    public int deleteCodeDictById(String codeType);
+
+    /**
+     * 批量删除业务码 
+     * 
+     * @param codeTypes 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteCodeDictByIds(String[] codeTypes);
+
+
+    public List<CodeDict> selectInCodeEnum(CodeEnumVo1 codeEnumVo1);
+}

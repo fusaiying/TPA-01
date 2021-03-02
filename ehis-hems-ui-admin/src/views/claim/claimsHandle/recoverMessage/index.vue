497c79d669a46375289e3522db5d55a95ff980e8
李盈财 <1446639902@qq.com>
2021-03-02 18:37:53 +0800

    2021/03/02

--- ehis-hems-ui-admin/src/views/claim/claimsHandle/recoverMessage/index.vue
+++ ehis-hems-ui-admin/src/views/claim/claimsHandle/recoverMessage/index.vue
@@ -104,10 +104,14 @@
           style=" width: 100%;">
           <el-table-column align="center" prop="rptNo" label="报案号" show-overflow-tooltip><!--超链接，点击可查看“理赔详情”页面-->
             <template slot-scope="scope">
-              <el-link style="font-size: 11px" type="primary" @click="claimRouter">{{scope.row.rptNo}}</el-link>
+              <el-link style="font-size: 11px" type="primary" @click="claimRouter(scope.row)">{{scope.row.rptNo}}</el-link>
             </template>
           </el-table-column>
-          <el-table-column align="center" prop="source" label="交单来源" show-overflow-tooltip/>
+          <el-table-column align="center" prop="source" label="交单来源" show-overflow-tooltip>
+            <template slot-scope="scope">
+              <span>{{selectDictLabel(delivery_sourceOptions, scope.row.source)}}</span>
+            </template>
+          </el-table-column>
           <el-table-column align="center" prop="hospitalCode" label="就诊医院" show-overflow-tooltip/>
           <el-table-column align="center" prop="insuredName" label="被保人姓名" width="90" show-overflow-tooltip/>
           <el-table-column align="center" prop="idNo" label="证件号码" show-overflow-tooltip/>
@@ -118,7 +122,11 @@
           <el-table-column align="center" prop="colAmount" label="收款金额" show-overflow-tooltip/>
           <el-table-column align="center" prop="residualAmount" label="剩余欠款CNY" width="100" show-overflow-tooltip/>
           <el-table-column align="center" prop="endCaseTime" label="结案日期" show-overflow-tooltip/>
-          <el-table-column align="center" prop="whiteStatus" label="白名单标记" width="90" show-overflow-tooltip/>
+          <el-table-column align="center" prop="whiteStatus" label="白名单标记" width="90" show-overflow-tooltip>
+            <template slot-scope="scope">
+              <span>{{selectDictLabel(white_list_labelOptions, scope.row.whiteStatus)}}</span>
+            </template>
+          </el-table-column>
           <el-table-column align="center" :formatter="getRiskStatus" prop="colStatus" label="状态"
                            show-overflow-tooltip/>
           <el-table-column align="center" label="操作">
@@ -174,7 +182,8 @@
   import moment from 'moment'
   import {initDebt, initReceipt} from '@/api/claim/recoverMessage'
   import {getListNew} from '@/api/insuranceRules/ruleDefin'
-  let dictss = [{dictType: 'product_status'}, {dictType: 'approvalconclusion'}, {dictType: 'white_list_label'},]
+  let dictss = [{dictType: 'product_status'}, {dictType: 'approvalconclusion'}, {dictType: 'white_list_label'},
+    {dictType: 'delivery_source'},{dictType: 'recover_status'},]
   export default {
     filters: {
       changeDate: function (value) {
@@ -219,6 +228,8 @@
         approvalconclusionOptions: [],
         sysUserOptions: [],
         white_list_labelOptions: [],
+        delivery_sourceOptions: [],
+        recover_statusOptions: [],
       }
     },
 
@@ -234,6 +245,12 @@
       }).dictDate
       this.white_list_labelOptions = this.dictList.find(item => {
         return item.dictType === 'white_list_label'
+      }).dictDate
+      this.delivery_sourceOptions = this.dictList.find(item => {
+        return item.dictType === 'delivery_source'
+      }).dictDate
+      this.recover_statusOptions = this.dictList.find(item => {
+        return item.dictType === 'recover_status'
       }).dictDate
       initDebt(this.queryParams).then(res => {
         if (res != null && res.code === 200) {
@@ -349,7 +366,7 @@
         })
       },
       getRiskStatus(row) {
-        return this.selectDictLabel(this.product_statusOptions, row.riskStatus)
+        return this.selectDictLabel(this.recover_statusOptions, row.colStatus)
       },
       remoteMethod(query) {
         if (query !== '' && query != null) {//调用特殊医院查询接口
@@ -366,7 +383,7 @@
       claimRouter(row){
         let data = encodeURI(
           JSON.stringify({
-            batchNo: '',
+            batchNo: row.batchNo,
             claimType: '',
             rptNo: row.rptNo,
             status:'show',

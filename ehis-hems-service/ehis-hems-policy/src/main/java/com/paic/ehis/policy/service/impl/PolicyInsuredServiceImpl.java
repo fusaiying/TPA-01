83c818a9742836850157dfab377bd0d761d8b872
王常磊 <wangchanglei@sinosoft.com.cn>
2021-03-08 15:57:11 +0800

    被保人信息查询

--- ehis-hems-service/ehis-hems-policy/src/main/java/com/paic/ehis/policy/service/impl/PolicyInsuredServiceImpl.java
+++ ehis-hems-service/ehis-hems-policy/src/main/java/com/paic/ehis/policy/service/impl/PolicyInsuredServiceImpl.java
@@ -1,5 +1,6 @@
 package com.paic.ehis.policy.service.impl;
 
+import com.paic.ehis.common.core.utils.StringUtils;
 import com.paic.ehis.policy.domain.PolicyInsured;
 import com.paic.ehis.policy.mapper.PolicyInsuredMapper;
 import com.paic.ehis.policy.service.IPolicyInsuredService;
@@ -16,8 +17,7 @@ import java.util.List;
  * @date 2021-01-09
  */
 @Service
-public class PolicyInsuredServiceImpl implements IPolicyInsuredService
-{
+public class PolicyInsuredServiceImpl implements IPolicyInsuredService {
     @Autowired
     private PolicyInsuredMapper policyInsuredMapper;
 
@@ -28,8 +28,7 @@ public class PolicyInsuredServiceImpl implements IPolicyInsuredService
      * @return 被保人信息
      */
     @Override
-    public PolicyInsured selectPolicyInsuredById(String insuredNo)
-    {
+    public PolicyInsured selectPolicyInsuredById(String insuredNo) {
         return policyInsuredMapper.selectPolicyInsuredById(insuredNo);
     }
 
@@ -40,9 +39,19 @@ public class PolicyInsuredServiceImpl implements IPolicyInsuredService
      * @return 被保人信息
      */
     @Override
-    public List<PolicyInsured> selectPolicyInsuredList(PolicyInsured policyInsured)
-    {
-        return policyInsuredMapper.selectPolicyInsuredList(policyInsured);
+    public List<PolicyInsured> selectPolicyInsuredList(PolicyInsured policyInsured) {
+        List<PolicyInsured> policyInsuredList = policyInsuredMapper.selectPolicyInsuredList(policyInsured);
+        if (!policyInsuredList.isEmpty()) {
+            policyInsuredMapper.selectPolicyInsuredList(policyInsured);
+        }
+        else {
+            for (PolicyInsured policyInsured1 : policyInsuredList) {
+            policyInsured1.setCreateTime(DateUtils.getNowDate());
+            policyInsured1.setUpdateTime(DateUtils.getNowDate());
+            policyInsuredMapper.insertPolicyInsured(policyInsured1);
+        }
+        }
+        return policyInsuredList;
     }
 
     /**
@@ -52,8 +61,7 @@ public class PolicyInsuredServiceImpl implements IPolicyInsuredService
      * @return 结果
      */
     @Override
-    public int insertPolicyInsured(PolicyInsured policyInsured)
-    {
+    public int insertPolicyInsured(PolicyInsured policyInsured) {
         policyInsured.setCreateTime(DateUtils.getNowDate());
         return policyInsuredMapper.insertPolicyInsured(policyInsured);
     }
@@ -65,8 +73,7 @@ public class PolicyInsuredServiceImpl implements IPolicyInsuredService
      * @return 结果
      */
     @Override
-    public int updatePolicyInsured(PolicyInsured policyInsured)
-    {
+    public int updatePolicyInsured(PolicyInsured policyInsured) {
         policyInsured.setUpdateTime(DateUtils.getNowDate());
         return policyInsuredMapper.updatePolicyInsured(policyInsured);
     }
@@ -78,8 +85,7 @@ public class PolicyInsuredServiceImpl implements IPolicyInsuredService
      * @return 结果
      */
     @Override
-    public int deletePolicyInsuredByIds(String[] insuredNos)
-    {
+    public int deletePolicyInsuredByIds(String[] insuredNos) {
         return policyInsuredMapper.deletePolicyInsuredByIds(insuredNos);
     }
 
@@ -90,8 +96,7 @@ public class PolicyInsuredServiceImpl implements IPolicyInsuredService
      * @return 结果
      */
     @Override
-    public int deletePolicyInsuredById(String insuredNo)
-    {
+    public int deletePolicyInsuredById(String insuredNo) {
         return policyInsuredMapper.deletePolicyInsuredById(insuredNo);
     }
 }

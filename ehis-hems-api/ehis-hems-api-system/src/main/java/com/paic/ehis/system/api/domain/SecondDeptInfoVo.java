bf46f202b8933ad928ce02a48f21a2fad90e9097
汪洁 <wangjie007@sinosoft.com.cn>
2021-03-10 16:19:23 +0800

    解决有效状态修改时，把正式表的数据状态变更了的问题

---
+++ ehis-hems-api/ehis-hems-api-system/src/main/java/com/paic/ehis/system/api/domain/SecondDeptInfoVo.java
@@ -0,0 +1,30 @@
+package com.paic.ehis.system.api.domain;
+
+import com.paic.ehis.common.core.annotation.Excel;
+import com.paic.ehis.common.core.web.domain.BaseEntity;
+import lombok.Data;
+
+/**
+ * order_info(工单信息)对象 order_info
+ * 
+ * @author sino
+ * @date 2021-01-26
+ */
+@Data
+public class SecondDeptInfoVo extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 二级科室名称 */
+    private String SecondDeptName;
+
+    /** 一级科室名称 */
+    @Excel(name = "一级科室名称")
+    private String firstDeptName;
+
+    /** 二级科室名称（B端） */
+    private String deptName;
+
+
+
+}

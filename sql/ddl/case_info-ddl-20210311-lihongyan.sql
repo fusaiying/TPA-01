045d3b309570bed47bd818f4ef4021ff2b0c6748
李洪岩 <2337121293@qq.com>
2021-03-11 17:46:57 +0800

    李洪岩

--- sql/ddl/case_info-ddl-20210311-lihongyan.sql
+++ sql/ddl/case_info-ddl-20210311-lihongyan.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS case_info;
 CREATE TABLE case_info
 (
-    pbw_case_id               BIGINT(20)     NOT NULL AUTO_INCREMENT COMMENT 'E结算案件信息ID' ,
-    rpt_no               VARCHAR(16)    NOT NULL COMMENT '报案号',
+    pbw_case_id               BIGINT(20)     NOT NULL  COMMENT 'E结算案件信息ID' ,
+    docu_no               VARCHAR(16)    NOT NULL COMMENT '报案号',
     batch_no             VARCHAR(32)    NOT NULL COMMENT '所属批次号',
     hospiral_id          VARCHAR(32)    NOT NULL COMMENT '医院ID',
     client_no            VARCHAR(32)    NOT NULL COMMENT '出险人客户号',
@@ -12,7 +12,7 @@ CREATE TABLE case_info
     pbw_batch_no         VARCHAR(20)    NOT NULL COMMENT 'pbw批次号',
     status               varchar(1)     NOT NULL COMMENT '数据状态(Y-成功,N-失败)',
     succ                 varchar(1)     COMMENT '数据状态(Y-成功,N-失败)',
-    message              varchar(32) COMMENT '返回信息',
+    message              varchar(2000) COMMENT '返回信息',
     create_by            VARCHAR(20)    NOT NULL COMMENT '创建人',
     create_time          DATETIME       NOT NULL COMMENT '创建时间',
     update_by            VARCHAR(20) COMMENT '更新人',

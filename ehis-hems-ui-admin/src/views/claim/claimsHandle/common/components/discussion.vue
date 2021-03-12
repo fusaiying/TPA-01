fbe36764c21fceb3e8f7d6f10f9081982356dac5
李盈财 <1446639902@qq.com>
2021-03-12 18:32:12 +0800

    2021/03/12 案件抽检

--- ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/discussion.vue
+++ ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/discussion.vue
@@ -1047,6 +1047,8 @@
               center: true,
               showClose: true
             });
+            this.$store.dispatch("tagsView/delView", this.$route);
+            this.$router.go(-1)
           }
         }).catch(res => {
           this.$message({

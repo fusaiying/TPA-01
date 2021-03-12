506d576d91d4a01b8bd2a9bae5288ac82be28137
hjw <houjiawei@sinosoft.como.cn>
2021-03-12 18:16:32 +0800

    最大金额最小金额

---
+++ sql/ddl/20210312-base_contract_service-ddl-houjiawei.sql
@@ -0,0 +1,5 @@
+
+ALTER TABLE base_contract_service MODIFY COLUMN min_price decimal(16) DEFAULT NULL COMMENT '最小金额';
+
+ALTER TABLE base_contract_service MODIFY COLUMN max_price decimal(16) DEFAULT NULL COMMENT '最大金';
+

d6cf72d49ae1ec8c43083e84701f074c7cbc4660
zyus <tylerzhouyusen@gmail.com>
2021-03-12 18:33:34 +0800

    质检处理详情页前端 zhouyus

--- ehis-hems-ui-admin/src/views/customService/common/moduel/personInfo.vue
+++ ehis-hems-ui-admin/src/views/customService/common/moduel/personInfo.vue
@@ -194,7 +194,7 @@ export default {
       console.info(getQuery);
       //查询外部接口获取保单和被保人信息
       getPersonInfoData(getQuery).then(res => {
-        console.log('------------: ', res)
+        console.log('++++++++------------: ', res)
         if (res !== undefined && res.code === 200) {
           if (res.total !== undefined && res.total >= 1) {
             this.personInfo = res.rows[0]

66ec90dd10a59692fa28935a1a66bb5ac84437de
王常磊 <wangchanglei@sinosoft.com.cn>
2021-03-17 18:43:19 +0800

    客服模块清单导出

--- ehis-hems-service/ehis-hems-cs/src/main/java/com/paic/ehis/cs/domain/vo/QualityAcceptVo.java
+++ ehis-hems-service/ehis-hems-cs/src/main/java/com/paic/ehis/cs/domain/vo/QualityAcceptVo.java
@@ -15,7 +15,7 @@ public class QualityAcceptVo {
     @Excel(name = "服务项目",readConverterExp = "01=增值服务类,02=理赔类,03=其他,04=VIP门诊预约,05=特殊门诊直接结算服务,06=门诊直接结算,07=住院直接结算,08=门诊中医治疗直接结算,B00006=投诉,B00034=根因改善")
     private String itemCode;
 
-    @Excel(name = "结案日期")
+    @Excel(name = "结案日期", dateFormat = "yyyy-MM-dd HH:mm:ss")
     private String endDate;
 
     @Excel(name = "质检分组")

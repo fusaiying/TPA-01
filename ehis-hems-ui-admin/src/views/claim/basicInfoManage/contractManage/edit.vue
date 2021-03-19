81e3ccbb4687b25ae384e5998d61a3648aa7f7eb
hjw <houjiawei@sinosoft.como.cn>
2021-03-19 10:13:20 +0800

    供应商服务项目  服务项目名称及 结算方式 不可重复

--- ehis-hems-ui-admin/src/views/claim/basicInfoManage/contractManage/edit.vue
+++ ehis-hems-ui-admin/src/views/claim/basicInfoManage/contractManage/edit.vue
@@ -883,6 +883,42 @@
           }
         }
       };
+      const checkReSerCode = (rule, value, callback) => {
+        if (!value) {
+          callback(new Error("服务项目不可为空"));
+        } else {
+          let listData = this.serverForm.serverInfo;
+          let count = 0;
+          for (let i = 0; i < listData.length; i++) {
+            if (listData[i].serviceCode == value) {
+              count++;
+            }
+          }
+          if (count > 1) {
+            callback(new Error("服务项目名称重复"));
+          } else {
+            callback();
+          }
+        }
+      };
+      const checkSettleType = (rule, value, callback) => {
+        if (!value) {
+          callback(new Error("结算方式不可为空"));
+        } else {
+          let listData = this.serverForm.serverInfo;
+          let count = 0;
+          for (let i = 0; i < listData.length; i++) {
+            if (listData[i].settleType == value) {
+              count++;
+            }
+          }
+          if (count > 1) {
+            callback(new Error("结算方式重复"));
+          } else {
+            callback();
+          }
+        }
+      };
       const checkReSupplierCode = (rule, value, callback) => {
         if (!value) {
           callback(new Error("供应商项目名称不可为空"));
@@ -1380,13 +1416,13 @@
           type: {trigger: ['change', 'blur'], required: true, message: '次均控费类型必填'},
         },
         serverInfoRules: {
-          serviceCode: [{required: true, message: '服务项名称', trigger: 'blur'}],
+          serviceCode: [{required: true, validator:checkReSerCode, trigger:['change', 'blur']}],
           supplierServiceName: [{required: true, validator: checkReSupplierCode, trigger: ['change', 'blur']}],
           minPrice: [{required: true, message: '最小价格', trigger: ['change', 'blur']}],
           maxPrice: [{required: true, validator: checkMaxPrice, trigger: ['change', 'blur']}],
-          limitnum: [{required: true, message: '日限次数', trigger: 'blur'}],
-          settleType: [{required: true, message: '结算方式', trigger: 'blur'}],
-          settleCurrency: [{required: true, message: '结算币种', trigger: 'blur'}],
+          limitnum: [{required: true, message: '日限次数', trigger: ['change', 'blur']}],
+          settleType: [{required: true, validator: checkSettleType, trigger: ['change', 'blur']}],
+          settleCurrency: [{required: true, message: '结算币种', trigger: ['change', 'blur']}],
         },
         dialogVisible: false,
         dateRange: '',

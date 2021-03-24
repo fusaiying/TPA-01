9d01ab902ce94f36125f497b6f2c4fff03e4f74d
hjw <houjiawei@sinosoft.como.cn>
2021-03-24 09:58:31 +0800

    申述案件赔付结论

--- ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/payConclusion.vue
+++ ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/payConclusion.vue
@@ -8,58 +8,47 @@
       </div>
     </div>
     <el-divider/>
-    <el-form ref="conclusionForm" :model="conclusionInfo" style="padding-bottom: 30px;margin-top:20px;margin-left: 5%"
-             label-width="130px"
-             label-position="right" size="mini">
+    <el-form ref="conclusionForm" :model="conclusionInfo" style="padding-bottom: 30px;margin-top:20px;margin-left: 5%"  label-width="130px" label-position="right" size="mini">
       <el-row>
         <el-col :span="8">
-          <span class="info_span_col to_right">账单金额：</span><span
-          class="info_span">{{ conclusionInfo.sumBillAmount }}</span>
+          <span class="info_span_col to_right">账单金额：</span> <span class="info_span">{{ conclusionInfo.sumBillAmount }} {{ conclusionInfo.sumBillAmount == ''  ? '' : billCurency }}</span>
         </el-col>
         <el-col :span="8">
-          <span class="info_span_col to_right">折扣金额：</span><span class="info_span money_class">{{ conclusionInfo.sumHosDiscountAmount }}</span>
+          <span class="info_span_col to_right">折扣金额：</span> <span class="info_span money_class">{{ conclusionInfo.sumHosDiscountAmount }} {{ conclusionInfo.sumHosDiscountAmount == ''  ? '' : billCurency}}</span>
         </el-col>
         <el-col :span="8">
-          <span class="info_span_col to_right">赔付金额：</span><span
-          class="info_span money_class">{{ conclusionInfo.payAmount }}</span>
+          <span class="info_span_col to_right">赔付金额：</span> <span class="info_span money_class">{{ conclusionInfo.calAmount }} {{ conclusionInfo.calAmount == '' || conclusionInfo.calAmount == null  ? '' : 'CNY' }}</span>
         </el-col>
       </el-row>
 
       <el-row>
         <el-col :span="8">
-          <span class="info_span_col to_right">拒赔金额：</span><span
-          class="info_span money_class">{{ conclusionInfo.refusedAmount }}</span>
+          <span class="info_span_col to_right">拒赔金额 ：</span> <span class="info_span money_class">{{ conclusionInfo.refusedAmount }} {{ conclusionInfo.refusedAmount == '' || conclusionInfo.refusedAmount == null ? '' : 'CNY' }}</span>
         </el-col>
         <el-col :span="8">
-          <span class="info_span_col to_right">追讨金额：</span><span class="info_span money_class">{{ conclusionInfo.debtAmount}}</span>
+          <span class="info_span_col to_right">追讨金额：</span> <span class="info_span money_class">{{ conclusionInfo.debtAmount}} {{ conclusionInfo.debtAmount == '' || conclusionInfo.debtAmount == null ? '' : 'CNY' }}</span>
         </el-col>
-        <!--<el-col :span="8">
-          <span class="info_span_col to_right">本次支付差额：</span><span
-          class="info_span_col">{{ conclusionInfo.amount}}</span>
-        </el-col>-->
       </el-row>
 
       <el-row>
         <el-col :span="8">
-          <span class="info_span_col to_right">账单币种：</span><span
-          class="info_span">{{ conclusionInfo.billCurrency }}</span>
+          <span class="info_span_col to_right">账单币种：</span> <span class="info_span">{{ conclusionInfo.billCurrency }}</span>
         </el-col>
         <el-col :span="8">
           <span class="info_span_col to_right">汇率：</span><span class="info_span money_class">{{ conclusionInfo.exchangeRate}}</span>
         </el-col>
         <el-col :span="8">
-          <span class="info_span_col to_right">外币给付金额：</span><span
-          class="info_span money_class">{{ conclusionInfo.payAmountForeign }}</span>
+          <span class="info_span_col to_right">外币给付金额：</span> <span class="info_span money_class">{{ conclusionInfo.payAmountForeign}}</span>
         </el-col>
       </el-row>
 
       <el-row>
         <el-col :span="8">
           <span class="info_span_col to_right">赔付结论：</span><span
-          class="info_span">{{ conclusionInfo.contractNo }}</span>
+          class="info_span">{{ conclusionInfo.payConclusion }}</span>
         </el-col>
         <el-col :span="8">
-          <span class="info_span_col to_right">拒赔原因：</span><span class="info_span">{{ conclusionInfo.amount}}</span>
+          <span class="info_span_col to_right">拒赔原因：</span><span class="info_span">{{ conclusionInfo.refusedReason}}</span>
         </el-col>
       </el-row>
 
@@ -67,7 +56,7 @@
         <el-col :span="12">
           <el-form-item label="客户备注：" prop="remark">
             <el-input disabled style="width:100%" col="2" type="textarea" row="4" maxlength="1000"
-                      v-model="conclusionInfo.customerRemark" class="item-width" clearable size="mini"
+                      v-model="conclusionInfo.remark" class="item-width" clearable size="mini"
                       placeholder=""/>
           </el-form-item>
         </el-col>
@@ -77,7 +66,7 @@
         <el-col :span="12">
           <el-form-item label="核赔依据：" prop="remark">
             <el-input disabled style="width:100%" col="2" type="textarea" row="4" maxlength="1000"
-                      v-model="conclusionInfo.fileRemark" class="item-width" clearable size="mini"
+                      v-model="conclusionInfo.claimCheck" class="item-width" clearable size="mini"
                       placeholder=""/>
           </el-form-item>
         </el-col>
@@ -95,21 +84,26 @@ export default {
   components: {},
   data() {
     return {
-      conclusionInfo: {
-        sumBillAmount:undefined,
-        sumHosDiscountAmount:undefined,
-        payAmount:undefined,
-        refusedAmount:undefined,
-        debtAmount:undefined,
-        billCurrency:undefined,
-        exchangeRate:undefined,
-        payAmountForeign:undefined,
+      //赔付结论信息 start
+      conclusionInfo:{
+        sumBillAmount:'', // 账单金额
+        sumHosDiscountAmount:'',// 折扣金额
+        payAmount:'', // 赔付金额
+        refusedAmount:'', //拒赔金额
+        debtAmount:'', // 追讨金额
+        billCurrency:'', // 账单币种
+        exchangeRate:'', // 汇率
+        payAmountForeign:'', // 账单币种
+        payConclusion:'', // 赔付结论
+        refusedReason:'', // 拒赔原因
+        remark:'',
+        claimCheck:'',
       },
+      //赔付结论信息 end
 
     }
   },
   mounted() {
- //console.log("this.fixInfo.rptNo", this.fixInfo.isAppeal)
     if(this.fixInfo.isAppeal === '01') {
      // console.log("this.fixInfo.rptNo.split('-')[0]", this.fixInfo.rptNo.split('-')[0])
       this.getCalInfo()

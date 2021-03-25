2ca13cef7b9716fab858df3d09a0ea7bd608b4e0
hjw <houjiawei@sinosoft.como.cn>
2021-03-25 14:44:05 +0800

    申诉纠错

--- ehis-hems-ui-admin/src/views/claim/claimsHandle/appeal/components/claimTable.vue
+++ ehis-hems-ui-admin/src/views/claim/claimsHandle/appeal/components/claimTable.vue
@@ -101,10 +101,10 @@ export default {
         this.$message({ type: 'warning',  message: '无法获取，该案件在申诉中'});
         return false;
       }
-      // if(row.payStatus !== '03') {
-      //   this.$message({ type: 'warning',  message: '该案件还未支付，请在支付环节进行回退操作，请核实。'});
-      //   return false;
-      // }
+      if(row.payStatus !== '03') {
+        this.$message({ type: 'warning',  message: '该案件还未支付，请在支付环节进行回退操作，请核实。'});
+        return false;
+      }
 
       const params = {};
       params.appealRptNo = row.rptNo;

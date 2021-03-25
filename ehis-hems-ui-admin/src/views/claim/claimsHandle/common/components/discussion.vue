6677be32f68ce58db83af9acf4b56ef9329f2650
hjw <houjiawei@sinosoft.como.cn>
2021-03-25 16:34:42 +0800

    赔付结论保存

--- ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/discussion.vue
+++ ehis-hems-ui-admin/src/views/claim/claimsHandle/common/components/discussion.vue
@@ -641,6 +641,7 @@
                 refusedReason:this.conclusionForm.refusedReason,
                 remark:this.conclusionForm.remark,
                 claimCheck:this.conclusionForm.claimCheck,
+                debtAmount:this.conclusionInfo.debtAmount
                 // calAmount:'0',
                 // payAmount:this.conclusionInfo.payAmount,
                 // refusedAmount:this.conclusionInfo.refusedAmount,

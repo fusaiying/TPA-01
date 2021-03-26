8d550bbae39fae4d53a7b9553b0192075e4959c7
wangss <GBG123456>
2021-03-26 17:28:51 +0800

    #000 -优化

--- ehis-hems-ui-admin/src/views/customService/common/components/workOrderTable.vue
+++ ehis-hems-ui-admin/src/views/customService/common/components/workOrderTable.vue
@@ -72,7 +72,7 @@
 
 <script>
   import {getMinData} from '@/api/claim/presentingReview'
-  import {updateGetWorkOrder,getWorkOrderFlag,editWorkOrder} from '@/api/customService/acceptQuery'
+  import {updateGetWorkOrder,getWorkOrderFlag,editWorkOrder,updateGetWorkOrder1} from '@/api/customService/acceptQuery'
   import {encrypt} from "@/utils/rsaEncrypt"
   import moment from "moment";
 
@@ -222,22 +222,36 @@
                 cancelButtonText: '取消',
                 type: 'warning'
               }).then(() => {
-                let url = '/customService/modify';
-                if (row.businessType == '03') {
-                  url = '/customService/complaint/modify';
-                } else if (row.businessType == '02') {
-                  url = '/customService/reservation/modify';
-                }
-                this.$router.push({
-                  path: url,
-                  query: {
-                    workOrderNo: row.workOrderNo,
-                    policyNo: row.policyNo,
-                    policyItemNo: row.policyItemNo,
-                    status: row.status,
-                    businessType: row.businessType
+                //已在个人池的数据需要先获取到本人名下再处理
+                updateGetWorkOrder1(row.workOrderNo).then(res => {
+                  if (res != null && res.code == '200') {
+                    let url = '/customService/modify';
+                    if (row.businessType == '03') {
+                      url = '/customService/complaint/modify';
+                    } else if (row.businessType == '02') {
+                      url = '/customService/reservation/modify';
+                    }
+                    this.$router.push({
+                      path: url,
+                      query: {
+                        workOrderNo: row.workOrderNo,
+                        policyNo: row.policyNo,
+                        policyItemNo: row.policyItemNo,
+                        status: row.status,
+                        businessType: row.businessType
+                      }
+                    })
+                  } else {
+                    this.$message({
+                      message: '获取失败!',
+                      type: 'error',
+                      center: true,
+                      showClose: true
+                    })
                   }
+                  this.$emit("searchHandle")
                 })
+
               }).catch(() => {
                 this.$message({
                   type: 'info',

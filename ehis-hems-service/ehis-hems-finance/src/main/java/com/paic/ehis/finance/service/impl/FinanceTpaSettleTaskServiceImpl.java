ecd9de11e7dae78857fc87d28d27935addf98916
flint <2395324399@qq.com>
2021-03-27 14:34:53 +0800

    2021/3/27

--- ehis-hems-service/ehis-hems-finance/src/main/java/com/paic/ehis/finance/service/impl/FinanceTpaSettleTaskServiceImpl.java
+++ ehis-hems-service/ehis-hems-finance/src/main/java/com/paic/ehis/finance/service/impl/FinanceTpaSettleTaskServiceImpl.java
@@ -200,6 +200,9 @@ public class FinanceTpaSettleTaskServiceImpl implements IFinanceTpaSettleTaskSer
                 tpaSettleInfo.setRiskName(baseIssuingRule.getRiskName());
                 tpaSettleInfo.setSettlementType(tpaSettleDTO.getSettlementType());
                 //设值 页面展示的服务费结算总金额
+                if (0 == tpaSettleInfo.getServiceSettleAmount().compareTo(new BigDecimal(String.valueOf(0)))){
+                    tpaSettleInfo.setServiceSettleAmount(new BigDecimal(String.valueOf(0)));
+                }
                 if ("01".equals(tpaSettleDTO.getSettlementType())) {
                     tpaSettleInfo.setServiceSettleAmount((baseIssuingRule.getSettlementvalue().multiply(new BigDecimal(companyRiskPolicy.getTotalPeople())))
                             .add(tpaSettleInfo.getServiceSettleAmount()));
@@ -276,7 +279,6 @@ public class FinanceTpaSettleTaskServiceImpl implements IFinanceTpaSettleTaskSer
             if ("01".equals(tpaSettleDTO.getSettlementType())){
                 tpaSettleInfo.setServiceSettleAmount(companyRule.getSettlementvalue().multiply(companyRiskPolicy.getSumPerm()));
             }
-
             if (!StringUtils.isNotNull(financeTpaSettleTask.getSettleStartDate())) {
                 financeTpaSettleTask.setSettleStartDate(companyRiskPolicy.getValidStartDate());
             }

53590ab5c43f078eb0f97db6342de5940beec76b
王常磊 <wangchanglei@sinosoft.com.cn>
2021-03-29 14:29:41 +0800

    抽检规则批处理

--- ehis-hems-service/ehis-hems-system/src/main/java/com/paic/ehis/system/task/CustomServiceTask.java
+++ ehis-hems-service/ehis-hems-system/src/main/java/com/paic/ehis/system/task/CustomServiceTask.java
@@ -58,4 +58,18 @@ public class CustomServiceTask {
             logger.error("每天12点将预约案件已处理改为已完成，结果：失败，原因：{}");
         }
     }
+
+    public void invalidcomplaint () {
+        String invalidDate = DateUtils.getDate();
+        logger.info("每周一凌晨4点将投诉案件进行抽检，当前日期{}", invalidDate);
+        AjaxResult ajaxResult = customServiceService.selectInvalidQiaMondayFour(invalidDate);
+        String jsonCode = JSON.toJSONString(ajaxResult.get(AjaxResult.CODE_TAG));
+        String dealCode = JSON.parseObject(jsonCode,String.class);
+        if(String.valueOf(HttpStatus.SUCCESS).equals(dealCode)){
+            logger.info("每周一凌晨4点将投诉案件进行抽检结束，结果：成功！");
+        }else{
+            String msg=JSON.toJSONString(ajaxResult.get(AjaxResult.MSG_TAG));
+            logger.error("每周一凌晨4点将投诉案件进行抽检已完成，结果：失败，原因：{}");
+        }
+    }
 }
